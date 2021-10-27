b1e37e101bf7583e74c78d7d4799c860ffa57b38
abulhasanmd <abulhasanmd@outlook.com>
2021-10-26 19:28:41 -0700

    Renamed Main and Test Class

--- AFourAirlines/backend/src/main/java/com/sjsu/cmpe202/AFourAirlinesApplication.java
+++ AFourAirlines/backend/src/main/java/com/sjsu/cmpe202/AFourAirlinesApplication.java
@@ -1,5 +1,7 @@
-package com.sjsu.cmpe255;
+package com.sjsu.cmpe202;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,14 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @RestController
-public class AfourairlinesApplication {
+public class AFourAirlinesApplication {
+
+	Logger LOG = LoggerFactory.getLogger(AFourAirlinesApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(AfourairlinesApplication.class, args);
+		SpringApplication.run(AFourAirlinesApplication.class, args);
 	}
 
 	@GetMapping("/health")
 	public String healthCheck(){
+		LOG.info("info log check");
 		return "Health is OK";
 	}
 
