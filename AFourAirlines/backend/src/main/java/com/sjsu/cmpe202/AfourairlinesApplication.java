901954f8300fc1ead7d492ab814d4acec6689841
abulhasanmd <abulhasanmd@outlook.com>
2021-10-26 19:12:13 -0700

    Few initialization changes
    1.Added HELP.md document
    2.Added health endpoint temporarily for health check
    3.Updated Artifact Name to AfourAirlines

--- AFourAirlines/backend/src/main/java/com/sjsu/cmpe202/AfourairlinesApplication.java
+++ AFourAirlines/backend/src/main/java/com/sjsu/cmpe202/AfourairlinesApplication.java
@@ -2,12 +2,20 @@ package com.sjsu.cmpe255;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@RestController
 public class AfourairlinesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AfourairlinesApplication.class, args);
+	}
+
+	@GetMapping("/health")
+	public String healthCheck(){
+		return "Health is OK";
 	}
 
 }
