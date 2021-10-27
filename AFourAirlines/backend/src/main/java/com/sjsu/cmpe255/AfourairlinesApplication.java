21afa2b7cad662a17e008c93ee929cf2466fae8f
abulhasanmd <abulhasanmd@outlook.com>
2021-10-26 18:48:38 -0700

    Created skeleton structure of the backend project

---
+++ AFourAirlines/backend/src/main/java/com/sjsu/cmpe255/AfourairlinesApplication.java
@@ -0,0 +1,13 @@
+package com.sjsu.cmpe255;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfourairlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfourairlinesApplication.class, args);
+	}
+
+}
---
+++ AFourAirlines/backend/src/test/java/com/sjsu/cmpe255/AfourairlinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjsu.cmpe255;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfourairlinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
