a855b9f127d141cab48de508913559c8804353bb
Rushi Sharma <rushisharma64@gmail.com>
2021-10-29 08:38:22 -0700

    Create search.js
    Added Search component folder and files

---
+++ AFourAirlines/frontend/afour-airlines/src/search/search.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+    class search {
+        render() {
+
+            return(
+                <div className="">
+                    
+
+
+
+
+                </div>
+            )
+        }
+
+
+    }
