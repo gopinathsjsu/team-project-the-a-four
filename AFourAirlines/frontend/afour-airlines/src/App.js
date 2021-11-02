4f0df71d98954ca0155792c4a350eb77209f65b8
Rushi Sharma <rushisharma64@gmail.com>
2021-11-02 11:53:41 -0700

    commit

--- AFourAirlines/frontend/afour-airlines/src/App.js
+++ AFourAirlines/frontend/afour-airlines/src/App.js
@@ -9,6 +9,7 @@ import BasePage from "./common/basepage";
 import MileageProgram from "./mileage/mileageProgram";
 import Search from "./search/search.js";
 import Login from "./login/login";
+import SearchPage from "./search/searchPage";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" exact render={(props) => <Search />} />
         <Route path="/mileageProgram" exact render={(props) => <MileageProgram />} />
         <Route path="/login" exact render={(props) => <Login />} />
+        <Route path="/searchPage" exact render={(props) => <SearchPage />} />
         </Switch>
       </Router>
     </BasePage>
