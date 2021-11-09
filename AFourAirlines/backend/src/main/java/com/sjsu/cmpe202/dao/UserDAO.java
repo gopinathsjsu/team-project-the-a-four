13078d13e3e54f05bf66fa49c958caf54b9ca964
Tuba Ahmed <md.tuba.ahmed@gmail.com>
2021-11-09 15:33:09 -0800

    Created basic structure of REST Api and implemented getter for User Entity

---
+++ AFourAirlines/backend/src/main/java/com/sjsu/cmpe202/dao/UserDAO.java
@@ -0,0 +1,12 @@
+package com.sjsu.cmpe202.dao;
+
+import com.sjsu.cmpe202.models.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDAO {
+
+    public User getUserDetails(String userName) {
+        return null;
+    }
+}
---
+++ AFourAirlines/backend/src/main/java/com/sjsu/cmpe202/service/UserService.java
@@ -0,0 +1,18 @@
+package com.sjsu.cmpe202.service;
+
+import com.sjsu.cmpe202.dao.UserRepository;
+import com.sjsu.cmpe202.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public Optional<User> getUserDetails(String userName) {
+        return userRepository.findById(userName);
+    }
+}
