d2f0f8a54e404908f276bd5912c0116878593785
eshasah <esha8sah@gmail.com>
2021-11-10 11:07:21 -0800

    register page modified

--- AFourAirlines/frontend/afour-airlines/src/login/register.js
+++ AFourAirlines/frontend/afour-airlines/src/login/register.js
@@ -1,33 +1,31 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import "../App.css";
-import NavBar from "../common/navbar";
 import {
   Button,
   FormControl,
   FormGroup,
-  FormHelperText,
-  MenuItem,
-  Select,
+  Input,
   TextField,
 } from "@material-ui/core";
 import Alert from "@material-ui/lab/Alert";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import moment from "moment";
 
 const Registration = () => {
   Axios.defaults.withCredentials = true;
   const [message, setMessage] = useState("");
-  const [membershipType, setMembershipType] = useState([]);
+  const [dob, setDate] = useState(new Date());
   const [invalid, setInvalid] = useState({
     // user_id: false,
     password: false,
     first_name: false,
     last_name: false,
     email_id: false,
-    zip_code: false,
-    street: false,
-    city: false,
+    country: false,
   });
   const defaultValues = {
     // user_id: "",
@@ -35,41 +33,33 @@ const Registration = () => {
     first_name: "",
     last_name: "",
     email_id: "",
-    zip_code: "",
-    street: "",
-    city: "",
-    membership_type: 0,
+    country: "",
   };
   const [userDetails, setUserDetails] = useState(defaultValues);
   const history = useHistory();
 
-  useEffect(() => {
-    axios.get("http://localhost:3001/getMembershipTypes").then((response) => {
-      if (response.data) {
-        setMembershipType(response.data);
-        console.log(response);
-      }
-    });
-  }, []);
+  
 
   const register = () => {
     if (
       userDetails.password.trim().length < 5 ||
       userDetails.first_name.trim() === "" ||
       userDetails.last_name.trim() === "" ||
-      userDetails.city.trim() === "" ||
-      userDetails.street.trim() === "" ||
+      userDetails.country.trim() === "" ||
       userDetails.email_id.trim() === "" ||
-      userDetails.zip_code.length < 5
+      userDetails.dob.trim() === ""
     ) {
       setMessage("Please fill all fields");
     } else if (
       userDetails.email_id.includes(" ") ||
-      userDetails.zip_code.includes(" ") ||
       userDetails.password.includes(" ")
     ) {
-      setMessage("Space character not allowed in zip_code, password, email_id");
-    } else {
+      setMessage("Space character not allowed in email_id and/or password");
+    } else if(
+      userDetails.dob.includes(" ")
+    ){
+      setMessage("Please select a valid date");
+    }else {
       Axios.post("http://localhost:3001/register", {
         userDetails
       })
@@ -129,26 +119,6 @@ const Registration = () => {
         <FormControl>
           <TextField
             required
-            helperText="Minimum 5 & Maximum 25 characters"
-            id="register-password"
-            label="Password"
-            type="password"
-            error={invalid.password}
-            onChange={(e) => {
-              const validation =
-                e.target.value.length < 5 ||
-                e.target.value.length > 25 ||
-                e.target.value === ""
-                  ? true
-                  : false;
-              setInvalid({ ...invalid, password: validation });
-              setUserDetails({ ...userDetails, password: e.target.value });
-            }}
-          />
-        </FormControl>
-        <FormControl>
-          <TextField
-            required
             helperText={invalid.email_id ? "1-25 characters" : ""}
             id="register-email-id"
             label="Email ID"
@@ -167,85 +137,48 @@ const Registration = () => {
         <FormControl>
           <TextField
             required
-            helperText={invalid.street ? "1-25 characters" : ""}
-            id="register-street"
-            label="Street"
-            type="text"
-            error={invalid.street}
+            helperText="Minimum 5 & Maximum 25 characters"
+            id="register-password"
+            label="Password"
+            type="password"
+            error={invalid.password}
             onChange={(e) => {
               const validation =
-                e.target.value.length > 25 || e.target.value === ""
+                e.target.value.length < 5 ||
+                e.target.value.length > 25 ||
+                e.target.value === ""
                   ? true
                   : false;
-              setInvalid({ ...invalid, street: validation });
-              setUserDetails({ ...userDetails, street: e.target.value });
+              setInvalid({ ...invalid, password: validation });
+              setUserDetails({ ...userDetails, password: e.target.value });
             }}
+          />
+        </FormControl>
+        <FormControl>
+          <Input
+            id="register-dob"
+            label="Date of Birth"
+            type="date"
+            required
           />
         </FormControl>
         <FormControl>
           <TextField
             required
-            helperText={invalid.city ? "1-25 characters" : ""}
-            id="register-city"
-            label="City"
+            helperText={invalid.country ? "1-25 characters" : ""}
+            id="register-country"
+            label="Country"
             type="text"
-            error={invalid.city}
+            error={invalid.country}
             onChange={(e) => {
               const validation =
                 e.target.value.length > 25 || e.target.value === ""
                   ? true
                   : false;
-              setInvalid({ ...invalid, city: validation });
-              setUserDetails({ ...userDetails, city: e.target.value });
+              setInvalid({ ...invalid, country: validation });
+              setUserDetails({ ...userDetails, country: e.target.value });
             }}
           />
-        </FormControl>
-        <FormControl>
-          <TextField
-            required
-            helperText="5 digit zip code"
-            id="register-zip-code"
-            label="ZIP Code"
-            type="number"
-            error={invalid.zip_code}
-            onChange={(e) => {
-              const validation =
-                e.target.value.length !== 5 || e.target.value === ""
-                  ? true
-                  : false;
-              setInvalid({ ...invalid, zip_code: validation });
-              setUserDetails({ ...userDetails, zip_code: e.target.value });
-            }}
-          />
-        </FormControl>
-        <FormControl>
-          <Select
-            id="register-member-type"
-            value={userDetails.membership_type}
-            onChange={(e) => {
-              setUserDetails({
-                ...userDetails,
-                membership_type: membershipType[e.target.value].type_id,
-              });
-            }}
-            defaultValue={0}
-            inputProps={{ "aria-label": "Without label" }}
-          >
-            {membershipType.map((each, index) => {
-              return (
-                <MenuItem key={each.type_id} value={index}>
-                  {each.name}
-                </MenuItem>
-              );
-            })}
-          </Select>
-          {membershipType.map((each) => {
-            return (
-              <FormHelperText key={each.type_id}>
-                {each.name + ": " + each.description}
-              </FormHelperText>
-            );
-          })}
         </FormControl>
         <FormControl>
         <div>
--- AFourAirlines/frontend/afour-airlines/src/mileage/mileageProgram.js
+++ AFourAirlines/frontend/afour-airlines/src/mileage/mileageProgram.js
@@ -18,6 +18,11 @@ class  MileageProgram extends Component {
                 <NavBar></NavBar>
                 <div>
                     <h1 style={{ textAlign: "center" }}>Mileage Program</h1>
+                    <h2>More places. More miles.
+                        More adventures.</h2>
+                    <p>MileageProgram is your ticket to the world, with the most ways to earn and use miles and the most award destinations of any U.S. airline loyalty program. 
+                        Now all you need is more vacation days.</p>
+                    <h2>Join our MileageProgram by filling just a few details!</h2>
                 </div>
                 <Registration></Registration>
             </div>
