9bbc511203faa2552d93586fa8e1c88f0c7afdf3
Rushi Sharma <rushisharma64@gmail.com>
2021-11-24 13:12:14 -0800

    added separate function to get flight data

--- AFourAirlines/frontend/afour-airlines/src/common/getFlightData.js
+++ AFourAirlines/frontend/afour-airlines/src/common/getFlightData.js
@@ -1,8 +1,32 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import FlightData from '../models/flightData';
 
 export const useGetFlightData = (props) => {
-    const [flightData, setFlightData] = useState({});
+
+
+    const defaultFlight = {
+        this.state.flightData.map(flight => {
+        flightID: flight.id,
+
+    };
+
+    const [flightData, setFlightData] = useState(defaultFlight);
     const { flightID } = useParams();
-}
+
+    axios.defaults.withCredentials = true;
+
+    useEffect(() => {
+        console.log(flightID);
+        // axios.get("http://localhost:3001/flght/get" + flightID).then((response) => {
+        //     if (response.data.********loggedIn************** === true) {
+        //       setFlightData(response.data.flight);
+        //     } else {
+        //       setFlightData({});
+        //     }
+        // });
+    }, []);
+
+    return { flightData };
+};
