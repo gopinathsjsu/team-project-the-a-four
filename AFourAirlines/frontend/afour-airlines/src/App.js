aa3ff37086927cb1166a151b62ddef538817aa7d
Rushi Sharma <rushisharma64@gmail.com>
2021-11-24 13:49:14 -0800

    updated app.js

--- AFourAirlines/frontend/afour-airlines/src/App.js
+++ AFourAirlines/frontend/afour-airlines/src/App.js
@@ -13,6 +13,7 @@ import FlightPayment from "./flights/flightPayment";
 import NewRegistration from "./login/newRegistration";
 import UserProfile from "./user/userProfile"
 import HelpPage from "./help/helpPage"
+import UserType from "./flights/userType";
 
 export default function App() {
 
@@ -22,8 +23,6 @@ export default function App() {
         <Switch>
         <Route path="/" exact render={(props) => <Search />} />
         <Route path="/mileageProgram" exact render={(props) => <MileageProgram />} />
-        <Route path="/:userName" exact render={(props) => <Search />} />
-        <Route path="/mileageProgram/:userName" exact render={(props) => <MileageProgram />} />
         <Route path ="/flightsList.js" exact render={(props) => <FlightsList/>} />
         <Route path ="/flightPayment" exact render={(props) => <FlightPayment/>} />
         <Route path ="/userType" exact render={(props) => <UserType/>} />
