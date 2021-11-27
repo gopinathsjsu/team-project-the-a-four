04e11fb5e7abe0cfd7c70823e607b93ba2d94c06
Tuba Ahmed <md.tuba.ahmed@gmail.com>
2021-11-27 12:23:45 -0800

    Correcting references in Reservation entity

--- AFourAirlines/backend/src/main/java/com/sjsu/cmpe202/models/Reservation.java
+++ AFourAirlines/backend/src/main/java/com/sjsu/cmpe202/models/Reservation.java
@@ -2,9 +2,7 @@ package com.sjsu.cmpe202.models;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -13,32 +11,19 @@ import javax.persistence.Id;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Reservation {
-    /*
-number
-int AI PK
-PNR
-varchar(5)
-passenger_id
-varchar(15)
-flight_id
-int
-seat_id
-int
-price
-int
-checked_in
-tinyint
-    */
     @Id
     private Integer number;
     @Column(name = "PNR")
     private String PNR;
-    @Column(name = "passenger_id")
-    private String passengerId;
-    @Column(name = "flight_id")
-    private Integer flightId;
-    @Column(name = "seat_id")
-    private Integer seatId;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "passenger")
+    private User passenger;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "flight")
+    private Flight flight;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "seat")
+    private Seat seat;
     @Column(name = "price")
     private Integer price;
     @Column(name = "status")
