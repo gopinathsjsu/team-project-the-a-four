86171a1cb95d6e6aa80e4002c6293a52ee38bfbb
Tuba Ahmed <md.tuba.ahmed@gmail.com>
2021-11-27 11:14:37 -0800

    Adding get-reservation-by-id

--- AFourAirlines/backend/src/main/java/com/sjsu/cmpe202/service/ReservationService.java
+++ AFourAirlines/backend/src/main/java/com/sjsu/cmpe202/service/ReservationService.java
@@ -1,8 +1,19 @@
 package com.sjsu.cmpe202.service;
 
+import com.sjsu.cmpe202.dao.FlightRepository;
+import com.sjsu.cmpe202.dao.ReservationRepository;
+import com.sjsu.cmpe202.models.Flight;
+import com.sjsu.cmpe202.models.Reservation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service @Slf4j
 public class ReservationService {
+    @Autowired
+    ReservationRepository reservationRepository;
+
+    public Optional<Reservation> getReservationById(Integer reservationId) { return reservationRepository.findById(reservationId); }
 }
