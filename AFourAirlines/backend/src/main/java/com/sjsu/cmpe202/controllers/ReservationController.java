e0b6e92aaf941dc3d19d1e714b851dd602f9f1e3
abulhasanmd <abulhasanmd@outlook.com>
2021-11-28 13:20:43 -0800

    Removed unused import

--- AFourAirlines/backend/src/main/java/com/sjsu/cmpe202/controllers/ReservationController.java
+++ AFourAirlines/backend/src/main/java/com/sjsu/cmpe202/controllers/ReservationController.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.ws.Response;
 
 @RestController
 @CrossOrigin
