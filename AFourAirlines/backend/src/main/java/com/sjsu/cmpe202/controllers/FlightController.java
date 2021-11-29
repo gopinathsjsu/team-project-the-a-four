a3c23d602f52bc15535617152fa0ec7ae473dc52
abulhasanmd <abulhasanmd@outlook.com>
2021-11-29 00:31:54 -0800

    Added loggers and refactored code for Apis

--- AFourAirlines/backend/src/main/java/com/sjsu/cmpe202/controllers/FlightController.java
+++ AFourAirlines/backend/src/main/java/com/sjsu/cmpe202/controllers/FlightController.java
@@ -1,10 +1,14 @@
 package com.sjsu.cmpe202.controllers;
 
 import com.sjsu.cmpe202.models.Flight;
+import com.sjsu.cmpe202.models.Reservation;
 import com.sjsu.cmpe202.service.FlightService;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Optional;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,32 +24,61 @@ public class FlightController {
 
     @GetMapping("/get-flight-details-by-id")
     public ResponseEntity<?> getFlightDetailsById(@RequestParam Integer flightId) throws Exception {
-        return ResponseEntity.ok(flightService.getFlightDetailsById(flightId));
+        log.info("Entering getFlightDetailsById Api");
+        Optional<Flight> flight = null;
+        try {
+            flight = flightService.getFlightDetailsById(flightId);
+        } catch (Exception e) {
+            log.error("Error occured in getReservationById :{}", e);
+            return ResponseEntity.status(400).body(e.getMessage());
+        } finally {
+            log.info("Exiting getFlightDetailsById Api");
+        }
+        return ResponseEntity.ok(flight);
     }
 
     @GetMapping("/get-flights")
     public ResponseEntity<?> getFlights(@RequestParam String sourceAirport, @RequestParam String destinationAirport, @RequestParam String departureDateString, @RequestParam String arrivalDateString) throws Exception {
-        LocalDate departureDate = LocalDate.parse(departureDateString);
-        LocalDate arrivalDate = LocalDate.parse(arrivalDateString);
-        return ResponseEntity.ok(flightService.getFlights(sourceAirport, destinationAirport, departureDate, arrivalDate));
+        log.info("Entering getFlights Api");
+        Optional<ArrayList<Flight>> flights = null;
+        try {
+            LocalDate departureDate = LocalDate.parse(departureDateString);
+            LocalDate arrivalDate = LocalDate.parse(arrivalDateString);
+            flights = flightService.getFlights(sourceAirport, destinationAirport, departureDate, arrivalDate);
+        } catch (Exception e) {
+            log.error("Error occured in getFlights :{}", e);
+            return ResponseEntity.status(400).body(e.getMessage());
+        } finally {
+            log.info("Exiting getFlights Api");
+        }
+        return ResponseEntity.ok(flights);
     }
 
     @PutMapping("/update-flight-details")
     public ResponseEntity<?> updateFlight(@RequestBody Flight flight) throws Exception {
-        log.info("flight is {}",flight);
-        return ResponseEntity.ok(flightService.save(flight));
+        log.info("Entering updateFlight Api");
+        String status = null;
+        try {
+            status = flightService.save(flight);
+        } catch (Exception e) {
+            log.error("Error occured in updateFlight :{}", e);
+            return ResponseEntity.status(400).body(e.getMessage());
+        } finally {
+            log.info("Exiting updateFlight Api");
+        }
+        return ResponseEntity.ok(status);
 
     }
 
     @GetMapping("/get-available-seats")
     public ResponseEntity<?> getAvailableSeats(@RequestParam Integer flightId) throws Exception {
-        log.info("flightiD is {}",flightId);
+        log.info("flightiD is {}", flightId);
         return ResponseEntity.ok(flightService.getAvailableSeats(flightId));
     }
 
     @GetMapping("/get-all-seats")
     public ResponseEntity<?> getAllSeats(@RequestParam Integer flightId) throws Exception {
-        log.info("flightiD is {}",flightId);
+        log.info("flightiD is {}", flightId);
         return ResponseEntity.ok(flightService.getAllSeats(flightId));
     }
 }
